24dd2dbbbcdb240ae55d0e669887f8fb2faf3c35
Patrick Vilchez <pvilchez794@gmail.com>
2022-03-11 14:27:43 -0500

    Image uploaded for linkedin presentation

--- pages/_document.js
+++ pages/_document.js
@@ -4,11 +4,11 @@ const Document = () => {
   return (
     <Html>
       <Head>
+        <meta name="image" property="og:image" content="/portrait.png" />
+        <meta name="author" content="Patrick Vilchez" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
         <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@300;400;700&display=swap" rel="stylesheet" />
-        {/* <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap" rel="stylesheet" /> */}
-        {/* <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;500;700&display=swap" rel="stylesheet"></link> */}
         <link rel="icon" href="/ellipsis.svg" />
       </Head>
 
