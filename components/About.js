62b5115459448e0675e61ed929c959099deb6e93
Patrick Vilchez <pvilchez794@gmail.com>
2022-03-11 00:34:51 -0500

    Other projects added, change font type and spacing

--- components/About.js
+++ components/About.js
@@ -5,13 +5,17 @@ const About = () => {
     <div className={style.about} id='about'>
       <h1 className={style.title}>About</h1>
       <p className={style.explanation}>
-        Actualmente me dedico al desarrollo web backend y frontend
-        con manejo de bases de datos relacionales y no relacionales.
-        Me enfoco en backend y tengo interés en administración de servidores y bases de datos.
-        Disfruto encontrarme con nuevos retos y como aprender a manejarlos.
+        I'm a Frontend and Backend web developer. I also know about
+        relational and non-relational databases as MongoDB and PostgreSQL.
+      </p>
+      <p className={style.explanation}>
+        I have knowledge on the Frontend side of JavaScript, CSS,
+        TailwindCSS, Socket.io and React. And on the Backend side of Python,
+        Django, Flask, and ExpressJS. I also have knowledge about Docker and AWS-S3.
+        I enjoy constantly learning and fecing new challenges.
       </p>
       <a className={style.download} href='https://drive.google.com/file/d/1JrJtZ61TluUh6ngVpU8JI0Oa-uNcKYFG/view?usp=sharing' target='_blank'>
-        Resume
+        Download Resume
       </a>
     </div>
   )
--- components/Projects.js
+++ components/Projects.js
@@ -1,6 +1,9 @@
 import Image from 'next/image'
 
 import style from '../styles/Projects.module.css'
+
+import chatRoomSs from '../public/chat-room.png'
+import contactsCalendarSs from '../public/contacts-calendar.png'
 import planetariumSs from '../public/planetarium.png'
 
 const Projects = () => {
@@ -15,16 +18,17 @@ const Projects = () => {
           <Image className={style.projectImg} src={planetariumSs} width={'500px'} height={'250px'} />
 
           <p className={style.projectExplanation}>
-            This page consume the NASA API for use images and descriptions for APOD.
+            This page consume the NASA API, use images and descriptions for A Picture Of the Day
+            and Mars Rover ordening and separating the images by their different cameras. Hosted in Vercel.
           </p>
 
           <div className={style.used}>
             <h4>Used technologies</h4>
-            <p>JavaScript React Styled-Components Docker</p>
+            <p>JavaScript | React | Styled-Components | Docker</p>
           </div>
 
           <div className={style.links}>
-            <a className={style.link} href='https://planetarium-nasa-api.vercel.app/' target='_blank'>
+            <a className={style.link} href='https://planetarium-nasa-api.vercel.app' target='_blank'>
               To page
             </a>
             <a className={style.link} href='https://github.com/Patrick564/planetarium-nasa-api' target='_blank'>
@@ -34,48 +38,50 @@ const Projects = () => {
         </div>
 
         <div className={style.project}>
-          <h3>Planetarium (NASA API)</h3>
+          <h3>Chat Room (Client)</h3>
 
-          <Image className={style.projectImg} src={planetariumSs} width={'500px'} height={'250px'} />
+          <Image className={style.projectImg} src={chatRoomSs} width={'500px'} height={'250px'} />
 
           <p className={style.projectExplanation}>
-            This page consume the NASA API for use images and descriptions for APOD.
+            A page created with a client (hosted in Vercel) and a server (hosted in Heroku)
+            to login with username and chat in realtime using Socket.io.
           </p>
 
           <div className={style.used}>
             <h4>Used technologies</h4>
-            <p>JavaScript React Styled-Components Docker</p>
+            <p>JavaScript | React | Styled-Components | Socket.io</p>
           </div>
 
           <div className={style.links}>
-            <a className={style.link} href='https://planetarium-nasa-api.vercel.app/' target='_blank'>
+            <a className={style.link} href='https://chat-room-client-psi.vercel.app' target='_blank'>
               To page
             </a>
-            <a className={style.link} href='https://github.com/Patrick564/planetarium-nasa-api' target='_blank'>
+            <a className={style.link} href='https://github.com/Patrick564/chat-room-client' target='_blank'>
               Repository
             </a>
           </div>
         </div>
 
         <div className={style.project}>
-          <h3>Planetarium (NASA API)</h3>
+          <h3>Contacts Calendar</h3>
 
-          <Image className={style.projectImg} src={planetariumSs} width={'500px'} height={'250px'} />
+          <Image className={style.projectImg} src={contactsCalendarSs} width={'500px'} height={'250px'} />
 
           <p className={style.projectExplanation}>
-            This page consume the NASA API for use images and descriptions for APOD.
+            A page created to login (with user and password) and save your contacts info
+            (name, birthday, phone number and email) in a database. Hosted in Heroku.
           </p>
 
           <div className={style.used}>
             <h4>Used technologies</h4>
-            <p>JavaScript React Styled-Components Docker</p>
+            <p>Django | TailwindCSS | JavaScript | SendGrid</p>
           </div>
 
           <div className={style.links}>
-            <a className={style.link} href='https://planetarium-nasa-api.vercel.app/' target='_blank'>
+            <a className={style.link} href='https://contacts-calendar.herokuapp.com' target='_blank'>
               To page
             </a>
-            <a className={style.link} href='https://github.com/Patrick564/planetarium-nasa-api' target='_blank'>
+            <a className={style.link} href='https://github.com/Patrick564/contacts_calendar' target='_blank'>
               Repository
             </a>
           </div>
