8c63fc651060ef93ff80d83764b0eadee75b39a5
Patrick Vilchez <pvilchez794@gmail.com>
2022-03-14 17:45:50 -0500

    Meta image changed from webp to png

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,8 @@ const Document = () => {
     <Html lang='en' translate='no'>
       <Head>
         <meta lang='en' content='notranslate' />
-        <meta name="image" property="og:image:secure_url" content="/portrait.webp" />
+        <meta name="image" property="og:image" content="/portrait.png" />
+        <meta name="image" property="og:image:secure_url" content="/portrait.png" />
         <meta name="author" content="Patrick Vilchez" />
         <meta name="description" content="A page to present my information, projects and contact data." />
         <link rel="icon" href="/ellipsis.svg" />
