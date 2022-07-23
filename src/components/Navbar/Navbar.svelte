29a075108328d530a42dbdd8c1eb5e37293c5bec
Patrick Vilchez <pvilchez794@gmail.com>
2022-07-23 00:09:03 -0500

    Pre final version

---
+++ src/components/Navbar/Navbar.svelte
@@ -0,0 +1,57 @@
+<script>
+  let tabs = [
+    { name: 'Home', to: '#presentation' },
+    { name: 'Projects', to: '#projects' },
+    { name: 'Contact', to: '#contact' }
+  ]
+</script>
+
+<nav class="navbar">
+  <ul class="navbar-items">
+    {#each tabs as tab}
+      <li>
+        <a href={tab.to}>{tab.name}</a>
+      </li>
+    {/each}
+  </ul>
+</nav>
+
+<style>
+  .navbar {
+    position: fixed;
+    min-width: 250px;
+    padding: 3px;
+    margin-top: 15px;
+    border-radius: 10px;
+    background-color: #111827;
+    left: 50%;
+    transform: translate(-50%, 0);
+    border: 2px solid #1e40af;
+    z-index: 1;
+  }
+
+  .navbar-items {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  li {
+    list-style: none;
+    margin: 0 2px;
+    border-radius: 10px;
+    cursor: pointer;
+  }
+
+  a {
+    color: #1e40af;
+    display: block;
+    padding: 10px 20px;
+    text-decoration: none;
+  }
+
+  a:visited {
+    color: #1e40af;
+  }
+</style>
